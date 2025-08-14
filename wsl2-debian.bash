10b6686fad75ecba50c848265cd02f6fd38b0f61
jsw <954951185@qq.com>
2025-08-15 00:40:59 +0800

    fix

--- wsl2-debian.bash
+++ wsl2-debian.bash
@@ -12,6 +12,9 @@ sudo apt upgrade -y
 # 部分图形界面 中文 显示问题
 sudo apt install -y fonts-wqy-microhei fonts-wqy-zenhei
 
+
+sudo apt install -y fonts-firacode
+
 # 刷新字体
 sudo fc-cache -f -v
 
@@ -22,16 +25,16 @@ sudo apt install vim -y
 sudo echo "set nu" | sudo tee -a /etc/vim/vimrc > /dev/null
 
 # 安装 man 中文 手册
-sudo apt install man-db manpages-zh
+sudo apt install -y man-db manpages-zh
 
 # 其他必要工具
-sudo apt install curl wget xz-utils
+sudo apt install -y curl wget xz-utils
 
 # 快速访问主机的下载文件目录 , jsw 为主机用户名
 sudo ln -s /mnt/c/Users/jsw/Downloads/ host_download
 
 # 编译 内核
-sudo apt install gcc make bc libssl-dev libelf-dev flex bison libncurses-dev
+sudo apt install -y gcc make bc libssl-dev libelf-dev flex bison libncurses-dev
 
 # 设置中文
 sudo dpkg-reconfigure locales
