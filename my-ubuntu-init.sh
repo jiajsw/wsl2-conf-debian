3989895baf688a2ac4b0ac48ce736ab4ce434e62
Jia <954951185@qq.com>
2026-02-20 16:27:36 +0800

    优化 my-ubuntu-init.sh，移除重复的 vim 行号设置

--- my-ubuntu-init.sh
+++ my-ubuntu-init.sh
@@ -7,11 +7,13 @@ PROMPT_COMMAND='PS1="$_OLD_PS1[$_MY_VAR_INIT]\n\\$ ";((_MY_VAR_INIT++))'
 sudo apt update && sudo apt upgrade -y
 
 sudo apt install -y vim fonts-firacode fonts-wqy-microhei fonts-wqy-zenhei 
+# 设置 vim 显示行号
+echo "set nu" | sudo tee -a /etc/vim/vimrc > /dev/null
 
 # 刷新字体
 sudo fc-cache -f -v
 
-# 设置 vim 显示行号
-echo "set nu" | sudo tee -a /etc/vim/vimrc > /dev/null
+
+
 
 
